baa928139c69327ee4bf05e1310bac2cfe3fdb0c
kwgon0212 <kwgon0102@gmail.com>
2025-06-23 15:35:07 +0900

    chore: Date format 변경함수 수정

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -3,7 +3,8 @@ export const formatDate = (dateString: string) => {
   const date = new Date(dateString);
   const diff = Math.floor((now.getTime() - date.getTime()) / 1000);
 
-  if (diff < 60) return `${diff}초 전`;
+  if (diff < 60) return "방금 전";
+  // if (diff < 60) return `${diff}초 전`;
   if (diff < 3600) return `${Math.floor(diff / 60)}분 전`;
   if (diff < 86400) return `${Math.floor(diff / 3600)}시간 전`;
   if (diff < 2592000) return `${Math.floor(diff / 86400)}일 전`;
