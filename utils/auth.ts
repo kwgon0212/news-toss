9ac91b75a1f767d7ea08648f21164ea3bf539f47
kwgon0212 <kwgon0102@gmail.com>
2025-06-24 14:28:15 +0900

    feat: 쿠키 JWT 검증 추가

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,8 +1,30 @@
+// "use server";
+
+// import { cookies } from "next/headers";
+// import { jwtDecode } from "jwt-decode";
+// import { JwtToken } from "@/type/jwt";
+
+// export async function getJwtToken() {
+//   const cookieStore = await cookies();
+//   const token = cookieStore.get("accessToken")?.value;
+
+//   if (!token) return null;
+
+//   try {
+//     const decoded: JwtToken = jwtDecode(token);
+//     return decoded;
+//   } catch {
+//     return null;
+//   }
+// }
+
 "use server";
 
 import { cookies } from "next/headers";
-import { jwtDecode } from "jwt-decode";
+import jwt from "jsonwebtoken";
 import { JwtToken } from "@/type/jwt";
+
+const SECRET = process.env.JWT_SECRET!;
 
 export async function getJwtToken() {
   const cookieStore = await cookies();
@@ -11,9 +33,16 @@ export async function getJwtToken() {
   if (!token) return null;
 
   try {
-    const decoded: JwtToken = jwtDecode(token);
+    const decoded = jwt.verify(token, SECRET) as JwtToken;
     return decoded;
-  } catch {
+  } catch (err) {
+    console.error("유효하지 않은 토큰입니당:", err);
+    cookieStore.set({
+      name: "accessToken",
+      value: "",
+      maxAge: 0,
+      path: "/",
+    });
     return null;
   }
 }
